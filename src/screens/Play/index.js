6ce9bbcc4c02d3c7331dacecdc3760d62e831e58
sunny <sunny.king1219@gmail.com>
2020-10-05 06:01:09 +0900

    sadga

--- src/screens/Play/index.js
+++ src/screens/Play/index.js
@@ -6,6 +6,7 @@ import {
     ImageBackground,
     StatusBar,
     NativeEventEmitter,
+    FlatList
 } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import { Images } from '../../config';
@@ -13,8 +14,6 @@ import Orientation from 'react-native-orientation';
 import { CateogryAction } from '../../actions';
 import RNDeviceRotation from 'react-native-device-rotation';
 
-const ReadyTime = 2;
-const StartTime = 2;
 export default class Play extends Component {
 
     constructor(props) {
@@ -22,14 +21,14 @@ export default class Play extends Component {
         this.state = {
             timer: 0,
             isReady: false,
-            isStart: false,
             isFinish: false,
             isPause: false,
             lstWord: null,
             currentWord: '',
             currentIndex: -1,
             detectDirection: "0",
-            background_image: Images.background_blue
+            background_image: Images.background_blue,
+            gotCardCount: 0,
         };
 
         Orientation.unlockAllOrientations();
@@ -37,7 +36,9 @@ export default class Play extends Component {
 
         RNDeviceRotation.setUpdateInterval(100);
 
-        this.orientationEvent = new NativeEventEmitter(RNDeviceRotation)
+        this.orientationEvent = new NativeEventEmitter(RNDeviceRotation);
+        this.ReadyTime = 2;
+        this.isTouchScreen = false;
 
         this.getWord();
     }
@@ -45,12 +46,15 @@ export default class Play extends Component {
     componentDidMount() {
         StatusBar.setHidden(true);
         this.orientationEvent.addListener('DeviceRotation', event => {
+            if (!this.state.isReady || this.state.isFinish)
+                return;
+
             const roll = Math.round(event.roll);
-            if (roll > 290 && this.state.currentWord != "CORRECT")
+            if (roll > 290 && !this.state.isPause)
                 this.correctAnswer();
-            else if (roll < 250 && this.state.currentWord != "FAILED")
+            else if (roll < 250 && !this.state.isPause)
                 this.failedAnswer();
-            else if (roll >= 250 && roll <= 290 && this.state.currentWord != "RESUME")
+            else if (roll >= 250 && roll <= 290 && this.state.isPause)
                 this.resumeGame();
         })
         RNDeviceRotation.start();
@@ -59,26 +63,74 @@ export default class Play extends Component {
     componentWillUnmount() {
         clearInterval(this.clockCall);
 
-        this.orientationEvent?.removeAllListeners();
         RNDeviceRotation.stop()
     }
 
-    pauseGame = () => {
-        this.setState({ isPause: true })
-    }
-
     resumeGame = () => {
-        this.setState({ currentWord: "RESUME", isPause: false, background_image: Images.background_blue })
+        this.setState({
+            isPause: false,
+            background_image: Images.background_blue,
+            currentIndex: (this.state.currentIndex + 1)
+        }, () => {
+            if (this.state.lstWord.length == this.state.currentIndex) {
+                this.finishTimer();
+                return;
+            }
+            this.setState({ currentWord: this.state.lstWord[this.state.currentIndex].name });
+        });
     }
 
     correctAnswer = () => {
-        this.pauseGame();
-        this.setState({ currentWord: "CORRECT", background_image: Images.background_green });
+        this.setState({
+            currentWord: "CORRECT",
+            background_image: Images.background_green,
+            isPause: true,
+        });
+        const words = this.state.lstWord;
+        words[this.state.currentIndex].mark = true;
+        this.setState({
+            lstWord: words,
+            gotCardCount: this.state.gotCardCount + 1
+        })
     }
 
     failedAnswer = () => {
-        this.pauseGame();
-        this.setState({ currentWord: "FAILED", background_image: Images.background_red });
+        this.setState({
+            currentWord: "PASS",
+            background_image: Images.background_red,
+            isPause: true,
+            gotCardCount: this.state.gotCardCount + 1
+        });
+
+        const words = this.state.lstWord;
+        words[this.state.currentIndex].mark = false;
+        this.setState({
+            lstWord: words
+        })
+    }
+
+    faildGuess = () => {
+        if (!this.state.isReady || this.state.isPause || this.state.isFinish || this.isTouchScreen)
+            return;
+
+        const words = this.state.lstWord;
+        words[this.state.currentIndex].mark = false;
+        this.setState({
+            lstWord: words,
+            gotCardCount: this.state.gotCardCount + 1
+        })
+
+        this.setState({
+            currentWord: "PASS",
+            background_image: Images.background_red,
+        }, () => {
+            this.isTouchScreen = true;
+            this.singleShot = setInterval(() => {
+                this.isTouchScreen = false;
+                clearInterval(this.singleShot);
+                this.resumeGame();
+            }, 500);
+        });
     }
 
     goHome() {
@@ -97,9 +149,8 @@ export default class Play extends Component {
 
     playGame = () => {
         this.setState({
-            timer: ReadyTime,
+            timer: this.ReadyTime,
             isReady: false,
-            isStart: false,
             isFinish: false,
             isPause: false,
             currentIndex: 0,
@@ -110,8 +161,7 @@ export default class Play extends Component {
 
     startTimer = () => {
         this.clockCall = setInterval(() => {
-            if (!this.state.isPause)
-                this.decrementClock();
+            this.decrementClock();
         }, 1000);
     }
 
@@ -120,15 +170,7 @@ export default class Play extends Component {
         if (!this.state.isReady) {
             this.setState({
                 isReady: true,
-                timer: StartTime,
-                currentWord: "LET'S START"
-            });
-            this.startTimer();
-        }
-        else if (!this.state.isStart) {
-            this.setState({
-                isStart: true,
-                timer: 10,
+                timer: 1,
                 currentWord: this.state.lstWord[this.state.currentIndex].name
             });
             this.startTimer();
@@ -137,62 +179,107 @@ export default class Play extends Component {
             this.setState({
                 isFinish: true,
                 timer: 0,
-                currentIndex: -1,
-                currentWord: "FINISHED!"
+                background_image: Images.background_blue,
+                currentWord: "FINISHED!",
+                gotCardCount: this.state.lstWord.filter(item => item.mark).length
             });
         }
     }
 
-    faildGuess = () => {
-        if (!this.state.isReady) {
-            this.finishTimer();
-            return;
-        }
-        if (this.state.isFinish || !this.state.isStart || this.state.isPause)
-            return;
-
-        this.setState({ currentIndex: this.state.currentIndex + 1 }, response => {
-            if (this.state.lstWord.length == this.state.currentIndex) {
-                this.finishTimer();
-                return;
-            }
-            this.setState({ currentWord: this.state.lstWord[this.state.currentIndex].name });
-        });
-    }
-
     decrementClock = () => {
-        if (this.state.timer === 0) {
+        if (this.isTouchScreen)
+            return;
+
+        if (this.state.timer == 0) {
             this.finishTimer();
             return;
         }
         this.setState((prevstate) => ({ timer: prevstate.timer - 1 }));
     };
 
+    renderItem = ({ item }) => {
+        return (
+            <View style={{ flex: 1, marginVertical: 5, justifyContent: "center", alignItems: "center" }}>
+                <Text style={{ color: item.mark ? "#53e25b" : "#b02a2b", fontSize: 30, fontWeight: "bold" }}>{item.name}</Text>
+            </View>
+        )
+    }
+
     render() {
         return (
             <View style={{ flex: 1, backgroundColor: "#000" }}>
                 <TouchableOpacity style={{ flex: 1 }} activeOpacity={1} onPress={this.faildGuess}>
                     <ImageBackground borderRadius={40} source={this.state.background_image} style={{ flex: 1, borderRadius: 50, borderColor: "#fff", borderWidth: 10 }}>
-                        <View style={{ width: "100%", height: "100%", padding: 40 }}>
-                            <View style={{ height: 40, flexDirection: "row" }}>
-                                <View style={{ flex: 1, alignItems: "flex-start" }}>
-                                    <TouchableOpacity onPress={this.goHome.bind(this)} activeOpacity={0.7} style={{
-                                        justifyContent: "center", alignItems: "center", backgroundColor: "#ffffff3f", borderRadius: 100,
-                                        width: 40, height: 40,
-                                    }}>
-                                        <Icon name={'angle-left'} color={'#fff'} size={30} />
-                                    </TouchableOpacity>
+                        <View style={{ width: "100%", height: "100%" }}>
+                            {this.state.isFinish ?
+                                <View style={{ height: 40, flexDirection: "row", margin: 25 }}>
+                                    <View style={{ flex: 1, alignItems: "flex-start" }}>
+                                        <TouchableOpacity onPress={this.goHome.bind(this)} activeOpacity={0.7} style={{
+                                            justifyContent: "center", alignItems: "center", backgroundColor: "#ffffff3f", borderRadius: 100,
+                                            width: 40, height: 40,
+                                        }}>
+                                            <Icon name={'angle-left'} color={'#fff'} size={30} />
+                                        </TouchableOpacity>
+                                    </View>
+                                    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", flexDirection: "row" }}>
+                                        <Text style={{ fontSize: 20, fontWeight: "bold", color: '#fff' }}>
+                                            YOU GOT
+                                        </Text>
+                                        <Text style={{ fontSize: 80, paddingHorizontal: 10, fontWeight: "bold", color: '#fff' }}>
+                                            {this.state.gotCardCount}
+                                        </Text>
+                                        <Text style={{ fontSize: 20, fontWeight: "bold", color: '#fff' }}>
+                                            CARDS
+                                        </Text>
+                                    </View>
+                                    <View style={{ flex: 1, justifyContent: "flex-end", alignItems: "flex-end" }}>
+                                        <TouchableOpacity onPress={this.goHome.bind(this)} activeOpacity={0.7} style={{
+                                            justifyContent: "center", alignItems: "center", backgroundColor: "#ffffff3f", borderRadius: 20,
+                                            paddingVertical: 10, paddingHorizontal: 20,
+                                        }}>
+                                            <Text style={{ color: "#fff", fontSize: 18, fontWeight: "bold" }}>PLAY</Text>
+                                        </TouchableOpacity>
+                                    </View>
                                 </View>
+                                :
+                                <View style={{ height: 40, flexDirection: "row", padding: 25 }}>
+                                    <View style={{ flex: 1, alignItems: "flex-start" }}>
+                                        <TouchableOpacity onPress={this.goHome.bind(this)} activeOpacity={0.7} style={{
+                                            justifyContent: "center", alignItems: "center", backgroundColor: "#ffffff3f", borderRadius: 100,
+                                            width: 40, height: 40,
+                                        }}>
+                                            <Icon name={'angle-left'} color={'#fff'} size={30} />
+                                        </TouchableOpacity>
+                                    </View>
+                                    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                                        <Text style={{ fontSize: 30, fontWeight: "bold", color: '#fff' }}>
+                                            {this.state.timer}
+                                        </Text>
+                                    </View>
+                                    <View style={{ flex: 1 }}></View>
+                                </View>}
+
+                            {this.state.isFinish &&
+                                <View style={{ backgroundColor: "#fff", height: 5, position: "absolute", top: 90, left: 0, width: "150%" }}></View>
+                            }
+                            {this.state.isFinish ?
+                                <View style={{ flex: 1, marginTop: 5 }}>
+                                    <FlatList
+                                        keyExtractor={(item, index) => index.toString()}
+                                        data={this.state.lstWord.filter(item => item.mark != null)}
+                                        renderItem={this.renderItem}
+                                        numColumns={2}
+                                        style={{ paddingTop: 20, paddingHorizontal: 10 }}
+                                    />
+                                    <View style={{ flex: 1, position: "absolute", top: 0, right: 0, left: 0, bottom: 0, backgroundColor: "#fff", opacity: 0.1, borderBottomLeftRadius: 46, borderBottomRightRadius: 46 }}>
+                                    </View>
+                                </View>
+                                :
                                 <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                                    <Text style={{ fontSize: 30, fontWeight: "bold", color: '#fff' }}>
-                                        {this.state.timer}
-                                    </Text>
+                                    <Text style={{ fontSize: 70, color: "#fff", fontWeight: "bold", marginBottom: 20 }}>{this.state.currentWord}</Text>
                                 </View>
-                                <View style={{ flex: 1 }}></View>
-                            </View>
-                            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                                <Text style={{ fontSize: 70, color: "#fff", fontWeight: "bold", marginBottom: 20 }}>{this.state.currentWord}</Text>
-                            </View>
+                            }
+
                         </View>
                     </ImageBackground>
                 </TouchableOpacity>
